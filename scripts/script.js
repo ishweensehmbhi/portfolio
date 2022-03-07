cb40da519c5ba651f3328ae5506b29f74c0c350d
ishween <ishweensehmbhi@gmail.com>
2022-03-07 01:48:06 -0500

    add easter eggs

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+const app = {};
+
+app.apiUrl =
+	"https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+
+app.listenForHover = () => {
+	const headerImage = document.querySelector(".header-img");
+	headerImage.addEventListener("mouseover", function () {
+		headerImage.src = "./assets/avatar-mod.svg";
+	});
+	headerImage.addEventListener("mouseleave", function () {
+		headerImage.src = "./assets/avatar-light.svg";
+	});
+};
+
+app.giveFunnyJoke = () => {
+	fetch(app.apiUrl)
+		.then(function (res) {
+			return res.json();
+		})
+		.then(function (jsonRes) {
+			if (jsonRes.type == "twopart") {
+				console.log(jsonRes.setup);
+				console.log(jsonRes.delivery);
+			} else if (jsonRes.type == "single") {
+				console.log(jsonRes.joke);
+			}
+		});
+};
+
+app.init = () => {
+	app.giveFunnyJoke();
+	app.listenForHover();
+};
+
+app.init();
