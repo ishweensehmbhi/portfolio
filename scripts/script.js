6a750118d587cac0b5110a9a1aa0bf308413ae62
ishween <ishweensehmbhi@gmail.com>
2022-03-07 14:43:30 -0500

    successfully prevent animations if user prefers reduced motion

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,11 @@ app.listenForHover = () => {
 	});
 };
 
+app.toggleDarkMode = () => {
+	const darkModeButton = document.querySelector(body);
+	darkModeButton.classList.toggle("dark-mode");
+};
+
 app.giveFunnyJoke = () => {
 	fetch(app.apiUrl)
 		.then(function (res) {
